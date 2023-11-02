c061808e56ac04746b6159bdf7d6d917fb26ec47
socram174 <socram174@hotmail.com>
2023-11-02 00:53:01 -0300

    Ejercicio 3

---
+++ ejercicio3/index.js
@@ -0,0 +1,6 @@
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre","Octubre", "Noviembre", "Diciembre"];
+
+
+meses.forEach((mes) => {
+    console.log(mes);
+});
